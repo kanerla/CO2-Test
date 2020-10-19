547f81cc7d4337a04f26c3b6dfe9596948808e5b
kanerla <kanervalaurajohanna@gmail.com>
2020-10-19 13:33:04 +0300

    Import Angular Material Modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,34 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { InfoComponent } from './info/info.component';
 import { InfoService } from './info.service';
+import { Co2Service } from './co2.service';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WasteComponent } from './waste/waste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    InfoComponent
+    InfoComponent,
+    WasteComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     MatTabsModule,
     MatIconModule,
+    MatRadioModule,
+    MatButtonModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule
   ],
-  providers: [InfoService],
+  providers: [InfoService, Co2Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
