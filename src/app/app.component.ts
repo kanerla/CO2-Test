073749786ab1d8e5bbd561def29f3801555c46e8
kanerla <kanervalaurajohanna@gmail.com>
2020-10-19 13:15:34 +0300

    Create frontpage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>TEST</h1>`,
+  template: `<div>
+  <app-header></app-header>
+  <mat-tab-group id="tabContainer">
+    <mat-tab>
+      <ng-template mat-tab-label>
+        <mat-icon class="tab-icon">info</mat-icon>
+        Info
+      </ng-template>
+      <app-info></app-info>
+    </mat-tab>
+
+    <mat-tab>
+      <ng-template mat-tab-label>
+        <mat-icon class="tab-icon">delete</mat-icon>
+        Waste
+      </ng-template>
+      Waste here
+    </mat-tab>
+
+    <mat-tab>
+      <ng-template mat-tab-label>
+        <mat-icon class="tab-icon">flight_takeoff</mat-icon>
+        Travel
+      </ng-template>
+      Travel here
+    </mat-tab>
+  </mat-tab-group>
+  </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
